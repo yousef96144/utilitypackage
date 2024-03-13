712cfb09f47dc2b09cd3b14992140d13e42ec1bc
yousef96144 <yousefelaidy24@gmail.com>
2024-03-14 00:29:58 +0200

    extra methods addes to uibutton

--- Sources/utility/Extinsion/UIButton.swift
+++ Sources/utility/Extinsion/UIButton.swift
@@ -11,16 +11,58 @@ extension UIButton {
     
     @IBInspectable public var setRadius: CGFloat{
         get {
-          return CGFloat(self.layer.cornerRadius)
+            return CGFloat(self.layer.cornerRadius)
         }set {
-          self.layer.cornerRadius = CGFloat(newValue)
+            self.layer.cornerRadius = CGFloat(newValue)
         }
     }
-    public func btncolor(color: UIColor){
-        self.backgroundColor = color
-    }
-    public func btntintcolor(tintcolor: UIColor){
-        self.tintColor = tintcolor
+    
+    @IBInspectable public var buttoncolor: UIColor{
+        get {
+            if let color = self.backgroundColor{
+                return color
+            }
+            else{
+                return .white
+            }
+        }
+        set {
+            self.backgroundColor = newValue
+        }
     }
     
+    @IBInspectable public var textcolor: UIColor{
+        get {
+            if let color = self.tintColor{
+                return color
+            }
+            else{
+                return .white
+            }
+        }
+        set {
+            self.tintColor = newValue
+        }
+    }
+    
+    @IBInspectable public var titleFont: UIFont? {
+        get { return titleLabel?.font }
+        set { titleLabel?.font = newValue }
+    }
+    @IBInspectable public var titleShadowColor: UIColor? {
+        get { return self.titleShadowColor(for: .normal) }
+        set {
+            setTitleShadowColor(newValue, for: .normal)
+        }
+    }
+    @IBInspectable public var setBackgroundImage: UIImage?{
+        get {
+            return self.backgroundImage(for: .normal)
+        }
+        set {
+            let image = newValue?.withRenderingMode(.alwaysOriginal)
+
+            setBackgroundImage(image, for: .normal)
+        }
+    }
 }
