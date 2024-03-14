f5d2453afff2176a2e345a3f0e14ac1eac127677
yousef96144 <yousefelaidy24@gmail.com>
2024-03-15 01:56:44 +0200

    tableview and scrollview updated

--- Sources/utility/Extinsion/ScrollView.swift
+++ Sources/utility/Extinsion/ScrollView.swift
@@ -8,131 +8,70 @@
 import Foundation
 import UIKit
 
-public extension UIScrollView {
-    fileprivate struct AssociatedKeys {
-        static var kKeyScrollViewVerticalIndicator = "_verticalScrollIndicator"
-        static var kKeyScrollViewHorizontalIndicator = "_horizontalScrollIndicator"
-    }
-    
-    ///  YES if the scrollView's offset is at the very top.
-    public var isAtTop: Bool {
-        get { return self.contentOffset.y == 0.0 ? true : false }
-    }
-    
-    ///  YES if the scrollView's offset is at the very bottom.
-    public var isAtBottom: Bool {
-        get {
-            let bottomOffset = self.contentSize.height - self.bounds.size.height
-            return self.contentOffset.y == bottomOffset ? true : false
-        }
-    }
-    
-    ///  YES if the scrollView can scroll from it's current offset position to the bottom.
-    public var canScrollToBottom: Bool {
-        get { return self.contentSize.height > self.bounds.size.height ? true : false }
-    }
-    
-    /// The vertical scroll indicator view.
-    public var verticalScroller: UIView {
-        get {
-            if (objc_getAssociatedObject(self, #function) == nil) {
-                objc_setAssociatedObject(self, #function, self.safeValueForKey(AssociatedKeys.kKeyScrollViewVerticalIndicator), objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN);
+
+    extension UIScrollView {
+
+        @IBInspectable public var paging: Bool{
+             get{
+                 return self.isPagingEnabled
+             }
+             set{
+                 self.isPagingEnabled = newValue
+             }
+         }
+        @IBInspectable public var contentsize: CGSize{
+             get{
+                 return self.contentSize
+             }
+             set {
+                 self.contentSize = newValue
+             }
+         }
+       @IBInspectable  public var scrolldelegate: UIScrollViewDelegate?{
+             get{
+                 return self.delegate
+             }
+             set {
+                 self.delegate = newValue
+             }
+         }
+         
+      
+        public var isAtTop: Bool {
+             get { return self.contentOffset.y == 0.0 ? true : false }
+         }
+        public var isAtBottom: Bool {
+               get {
+                   let bottomOffset = self.contentSize.height - self.bounds.size.height
+                   return self.contentOffset.y == bottomOffset ? true : false
+               }
+           }
+        func scrolltoview(view:UIView, animated: Bool) {
+            if let origin = view.superview {
+                let childStartPoint = origin.convert(view.frame.origin, to: self)
+               
+                self.scrollRectToVisible(CGRect(x:0, y:childStartPoint.y,width: 1,height: self.frame.height), animated: animated)
             }
-            return objc_getAssociatedObject(self, #function) as! UIView
         }
-    }
 
-    /// The horizontal scroll indicator view.
-    public var horizontalScroller: UIView {
-        get {
-            if (objc_getAssociatedObject(self, #function) == nil) {
-                objc_setAssociatedObject(self, #function, self.safeValueForKey(AssociatedKeys.kKeyScrollViewHorizontalIndicator), objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN);
+        func scrolltotop(animated: Bool) {
+            let topOffset = CGPoint(x: 0, y: -contentInset.top)
+            setContentOffset(topOffset, animated: animated)
+        }
+
+        func scrolltobottom() {
+            let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height + contentInset.bottom)
+            if(bottomOffset.y > 0) {
+                setContentOffset(bottomOffset, animated: true)
             }
-            return objc_getAssociatedObject(self, #function) as! UIView
+        }
+        
+        public func scrollToTopAnimated(_ animated: Bool) {
+            if !self.isAtTop {
+                let bottomOffset = CGPoint.zero;
+                self.setContentOffset(bottomOffset, animated: animated)
+            }
         }
     }
     
-    fileprivate func safeValueForKey(_ key: String) -> AnyObject{
-        let instanceVariable: Ivar = class_getInstanceVariable(type(of: self), key.cString(using: String.Encoding.utf8)!)!
-        return object_getIvar(self, instanceVariable) as AnyObject;
-    }
-    
 
-     /**
-     Sets the content offset to the top.
-     
-     - parameter animated: animated YES to animate the transition at a constant velocity to the new offset, NO to make the transition immediate.
-     */
-    public func scrollToTopAnimated(_ animated: Bool) {
-        if !self.isAtTop {
-            let bottomOffset = CGPoint.zero;
-            self.setContentOffset(bottomOffset, animated: animated)
-        }
-    }
-
-    /**
-     Sets the content offset to the bottom.
-     
-     - parameter animated: animated YES to animate the transition at a constant velocity to the new offset, NO to make the transition immediate.
-     */
-    public func scrollToBottomAnimated(_ animated: Bool) {
-        if self.canScrollToBottom && !self.isAtBottom {
-            let bottomOffset = CGPoint(x: 0.0, y: self.contentSize.height - self.bounds.size.height)
-            self.setContentOffset(bottomOffset, animated: animated)
-        }
-    }
-
-    /**
-      Stops scrolling, if it was scrolling.
-     */
-    public func stopScrolling() {
-        guard self.isDragging else {
-            return
-        }
-        var offset = self.contentOffset
-        offset.y -= 1.0
-        self.setContentOffset(offset, animated: false)
-
-        offset.y += 1.0
-        self.setContentOffset(offset, animated: false)
-    }
-}
-
-
-
-
-public extension UIScrollView {
-    
-    public convenience init(frame: CGRect, contentSize: CGSize, clipsToBounds: Bool, pagingEnabled: Bool, showScrollIndicators: Bool, delegate: UIScrollViewDelegate?) {
-        self.init(frame: frame)
-        self.delegate = delegate
-        self.isPagingEnabled = pagingEnabled
-        self.clipsToBounds = clipsToBounds
-        self.showsVerticalScrollIndicator = showScrollIndicators
-        self.showsHorizontalScrollIndicator = showScrollIndicators
-        self.contentSize = contentSize
-    }
-    
-    public var isOverflowVertical: Bool {
-        return self.contentSize.height > bounds.height && bounds.height > 0
-    }
-    
-    public func isReachedBottom(withTolerance tolerance: CGFloat = 0) -> Bool {
-        guard self.isOverflowVertical else { return false }
-        let contentOffsetBottom = self.contentOffset.y + bounds.height
-        return contentOffsetBottom >= self.contentSize.height - tolerance
-    }
-    
-    public func scrollToBottom(animated: Bool) {
-        guard self.isOverflowVertical else { return }
-        let targetY = self.contentSize.height + self.contentInset.bottom - bounds.height
-        let targetOffset = CGPoint(x: 0, y: targetY)
-        self.setContentOffset(targetOffset, animated: true)
-    }
-    
-    public func scrollToTop(animated: Bool = true) {
-        let inset = contentInset
-        setContentOffset(CGPoint(x: -inset.left, y: -inset.top), animated: animated)
-    }
-    
-}
